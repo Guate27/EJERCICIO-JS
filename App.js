71a33279d4e6598bae046d08af44f1d1ba8dd8bb
Guate27 <reypellecer@gmail.com>
2026-03-14 23:44:18 -0600

    Implementación de la función mostrarEtiquetas solicitada en la sección B

--- App.js
+++ App.js
@@ -1,1 +1,23 @@
 const habilidades = ["PYTHON", "LINUX", "IA", "N8N", "APIS"];
+
+function mostrarEtiquetas (habilidades) {
+    habilidades.forEach ( (habilidad) => {
+
+        const nuevo_spanner = document.createElement("span");
+
+        nuevo_spanner.class.add("etiqueta");
+
+        nuevo_spanner.textContent = habilidad;
+
+        const contenedor_etiquetas = document.getElementById("etiquetas");
+
+        contenedor_etiquetas.appendChild(nuevo_spanner);
+
+
+        
+
+
+
+
+    })
+} 
